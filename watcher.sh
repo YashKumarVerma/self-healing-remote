a63125dc8f1d160eb688c93ed3d63f09ed7ff219
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-11-07 09:09:20 +0530

    Created Checkpoint

---
+++ watcher.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# clean cloud links, initialize working space again
+mkdir -p disk/ >> lastCommand.txt
+rm -rf disk/ >> lastCommand.txt
+rm -rf .git >> lastCommand.txt
+git init >> lastCommand.txt
+git remote add origin https://github.com/YashKumarVerma/self-healing-remote >> lastCommand.txt
+git pull origin master --force >> lastCommand.txt
+mkdir -p disk >> lastCommand.txt
+
+# hide ctrl+c from terminal
+stty -echoctl 
+
+# function called by trap
+other_commands() {
+    tput setaf 1
+    printf "\r System Crashed !\n"
+
+    # reset to last checkpoint
+    printf "Since system crashed, reverting to last checkpoint"
+    git reset --hard HEAD >> lastCommand.txt
+    if [ $? -eq 0 ]; then
+        printf "Successfully restored last checkpoint state\n"
+    else
+        printf "Error restoring last checkpoint\n"
+        exit;
+    fi
+
+
+    printf "\rTaking system to last made checkpoint\n"
+    tput sgr0
+    sleep 3
+    ./selfHealing
+}
+
+trap 'other_commands' SIGINT
+input="$@"
+while true; do
+    ./selfHealing
+    read input
+    [[ $input == close ]] && break
+    bash -c "$input"
+done
